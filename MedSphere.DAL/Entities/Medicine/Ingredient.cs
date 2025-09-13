c3e16dd63004c08a591c24b9063ad73306330112
Ramez <ramezemad44@gmail.com>
2025-09-13 22:03:49 +0300

    CreateDataBase and Medicine Tables

---
+++ MedSphere.DAL/Entities/Medicine/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedSphere.DAL.Entities.Medicine;
+public class Ingredient
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public ICollection<MedicineIngredient> MedicineIngredients { get; set; } = new HashSet<MedicineIngredient>();
+}
