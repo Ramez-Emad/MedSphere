1a5d8f41164f250d3dc36982ed84dc33ab4e50de
Ramez <ramezemad44@gmail.com>
2025-09-24 13:06:32 +0300

    Update format

--- MedSphere.DAL/Repositories/Ingredients/IngredientRepository.cs
+++ MedSphere.DAL/Repositories/Ingredients/IngredientRepository.cs
@@ -8,10 +8,7 @@ public class IngredientRepository : GenericRepository<Ingredient>, IIngredientRe
 {
     private readonly AppDbContext _appDbContext;
 
-    public IngredientRepository(AppDbContext appDbContext) : base(appDbContext)
-    {
-        _appDbContext = appDbContext;
-    }
+    public IngredientRepository(AppDbContext appDbContext) : base(appDbContext) => _appDbContext = appDbContext;
 
     public async Task<int?> IsIngredientNameExists(string name, CancellationToken cancellationToken = default)
        => await _appDbContext.Ingredients.Where(i => i.Name == name).Select(i => i.Id).FirstOrDefaultAsync(cancellationToken);
